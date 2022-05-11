22ce562c7ded3b8f091e8c0142fafcfc68638546
Andrei Bogdan <dei7bogdan@gmail.com>
2022-05-11 20:07:14 +0300

    modified body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import './Body.css'
+import React from "react";
+import "./Body.css";
 
 function Body() {
   return (
-    <div className='body'>
-      <img id='user_img' height={150} src="./Images/user.jpg" alt="User" />
-      <br /><br /><br /><br /><br /><br /><br /><br />
+    <div className="body">
+      <img id="user_img" height={150} src="./Images/user.jpg" alt="User" />
+      <br />
+      <br />
+      <br />
       <h1>Welcome to the website</h1>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
