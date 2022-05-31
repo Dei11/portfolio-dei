931d1fb26af76560513d09beee2a16d938a9ef7c
Andrei Bogdan <dei7bogdan@gmail.com>
2022-05-31 19:42:48 +0300

    video bg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Body from "../components/Body";
-import videoBg from "../video/video.mp4";
+import videoBg from "../assets/video.mp4";
+import "./Home.css";
 
 function Home() {
   return (
     <div className="main">
-      <Body />
+      {/* <Body /> */}
+      <div className="overlay"></div>
       <video src={videoBg} autoPlay loop muted />
+      <div className="content">
+        <h1 className="text-4xl">Hello, I'm Andrei</h1>
+        <br />
+        <h2 className="text-2xl">contact me</h2>
+      </div>
     </div>
   );
 }
